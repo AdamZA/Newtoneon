8a505ed237e982c5d8f4a20aa1f53556cc41a1f3
Adam Home <adamrayner@live.com>
2017-01-04 23:04:30 +0200

    Copied in proper textures. Orbital animation complete.

---
+++ 2ndLaw/Assets/Scripts/RotationalScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationalScript : MonoBehaviour
+{
+    //Object references
+    public Transform playerObject;
+
+    //Class Variables
+    private float speed = 2.0f;
+    private Vector2 _centre;
+    private float _angle;
+    private float _radius;
+
+    // Use this for initialization
+    void Start ()
+    {
+        _angle = 0;
+        _centre = playerObject.transform.position;
+        _radius = 0.7f;
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        _centre = playerObject.transform.position;
+        _angle += speed * -Time.deltaTime; //if you want to switch direction, use -= instead of +=
+        var x = Mathf.Cos(_angle);
+        var y = Mathf.Sin(_angle);
+        transform.localPosition = new Vector3(x* _radius, y * _radius, transform.localPosition.z);
+    }
+}
