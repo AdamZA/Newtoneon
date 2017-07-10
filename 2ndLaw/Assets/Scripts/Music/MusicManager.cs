574438defef98642275105cf71bd3f8bac8395b7
Adam Home <adamrayner@live.com>
2017-07-10 21:54:29 +0200

    WIP looping music

---
+++ 2ndLaw/Assets/Scripts/Music/MusicManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicManager : MonoBehaviour {
+
+    public AudioClip initialMusic;
+    public AudioClip loopMusic;
+    private AudioSource _source;
+    private float _delay;
+
+	// Use this for initialization
+	void Start ()
+    {
+        _source = GetComponent<AudioSource>();
+        _delay = initialMusic.length;
+        playMusic();
+    }
+
+    public void playMusic()
+    {
+        _source.clip = initialMusic;
+        _source.Play();
+        Invoke("transitionMusic", _delay);
+    }
+
+    private void transitionMusic()
+    {
+        _source.Stop();
+        _source.clip = loopMusic;
+        _source.loop = true;
+        _source.Play();
+    }
+}
